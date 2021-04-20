eca3b5041bb1c5a28a539c8a7d84864f3697782a
rmflight <r.flight79@gmail.com>
2021-04-20 15:21:40 -0400

    made a wrapper to run things in multicore for base kendall

--- R/selection_functions.R
+++ R/selection_functions.R
@@ -80,7 +80,7 @@ run_fractional_kendall = function(in_data, replace_0 = FALSE){
     in_data$data = tmp_data
   }
   t1 = Sys.time()
-  out_cor = cor(in_data$data, method = "kendall", use = "pairwise.complete.obs")
+  out_cor = multicore_kendallt(in_data$data)
   t2 = Sys.time()
   t_res = as.numeric(difftime(t2, t1, units = "secs"))
   res = list(cor = out_cor,
@@ -88,4 +88,65 @@ run_fractional_kendall = function(in_data, replace_0 = FALSE){
              type = in_data$type,
              frac = in_data$frac)
   res
+}
+
+multicore_kendallt = function(data_matrix){
+  
+  n_sample = ncol(data_matrix)
+  # set everything to NA and let R take care of it
+  
+  ncore = future::nbrOfWorkers()
+  names(ncore) = NULL
+  
+  pairwise_comparisons = combn(n_sample, 2)
+  
+  n_todo = ncol(pairwise_comparisons)
+  n_each = ceiling(n_todo / ncore)
+  
+  split_comparisons = vector("list", ncore)
+  start_loc = 1
+  
+  for (isplit in seq_along(split_comparisons)) {
+    stop_loc = min(start_loc + n_each, n_todo)
+    
+    split_comparisons[[isplit]] = pairwise_comparisons[, start_loc:stop_loc, drop = FALSE]
+    start_loc = stop_loc + 1
+    
+    if (start_loc > n_todo) {
+      break()
+    }
+  }
+  
+  null_comparisons = purrr::map_lgl(split_comparisons, is.null)
+  split_comparisons = split_comparisons[!null_comparisons]
+  
+  do_split = function(do_comparisons, exclude_data, perspective) {
+    #seq_range = seq(in_range[1], in_range[2])
+    #print(seq_range)
+    tmp_cor = matrix(0, nrow = ncol(data_matrix), ncol = ncol(data_matrix))
+    rownames(tmp_cor) = colnames(tmp_cor) = colnames(data_matrix)
+    
+    for (icol in seq(1, ncol(do_comparisons))) {
+      iloc = do_comparisons[1, icol]
+      jloc = do_comparisons[2, icol]
+      tmp_cor[iloc, jloc] = tmp_cor[jloc, iloc] = cor(x = data_matrix[, iloc], y = data_matrix[, jloc], use= "pairwise.complete.obs", method = "kendall")
+    }
+    
+    tmp_cor
+  }
+  # we record how much time is actually spent doing ICI-Kt
+  # itself, as some of the other operations will add a bit of time
+  # 
+  t1 = Sys.time()
+  split_cor = furrr::future_map(split_comparisons, do_split, exclude_data, perspective)
+  t2 = Sys.time()
+  t_diff = as.numeric(difftime(t2, t1, units = "secs"))
+  
+  cor_matrix = matrix(0, nrow = ncol(exclude_data), ncol = ncol(data_matrix))
+  rownames(cor_matrix) = colnames(cor_matrix) = colnames(data_matrix)
+  for (isplit in split_cor) {
+    cor_matrix = cor_matrix + isplit
+  }
+  
+  cor_matrix
 }
