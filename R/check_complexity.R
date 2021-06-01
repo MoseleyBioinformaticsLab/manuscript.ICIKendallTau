bbf16ee76a60f2684775b59df92c2ead58e143a0
rmflight <r.flight79@gmail.com>
2021-06-01 10:06:38 -0400

    including the algorithmic complexity stuff

--- R/check_complexity.R
+++ R/check_complexity.R
@@ -4,21 +4,27 @@ create_complexity_figure = function(single_core_perf){
   
   p_comp = ggplot(pearson, aes(x = n, y = time)) + geom_line() +
     geom_smooth(method = "lm", formula = y ~ (x)) +
-    labs(subtitle = "r_pearson, O(n)")
+    labs(subtitle = "r_pearson, O(n)",
+         x = "Number of Features",
+         y = "time (s)")
   
   kendall = single_core_perf %>%
     dplyr::filter(method %in% "r_kendall")
   
   k_comp = ggplot(kendall, aes(x = n, y = time)) + geom_line() +
     geom_smooth(method = "lm", formula = y ~ I(x^2)) +
-    labs(subtitle = "r_kendall, O(n^2)")
+    labs(subtitle = "r_kendall, O(n^2)",
+         x = "Number of Features",
+         y = "time (s)")
   
   ici = single_core_perf %>%
     dplyr::filter(method %in% "ici_kt")
   
   ici_comp = ggplot(ici, aes(x = n, y = time)) + geom_line() +
     geom_smooth(method = "lm", formula = y ~ x * log(x)) +
-    labs(subtitle = "ici_kt, O(nlog(n))")
+    labs(subtitle = "ici_kt, O(nlog(n))",
+         x = "Number of Features",
+         y = "Time (s)")
   
   p_comp / ici_comp / k_comp
   
