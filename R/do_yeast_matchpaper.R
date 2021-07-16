7fa2e1c94a330ee11415416d2c1d555a7af38a7c
rmflight <r.flight79@gmail.com>
2021-07-16 11:26:03 -0400

    try to avoid -Inf so we just remove the zero counts

--- R/do_yeast_matchpaper.R
+++ R/do_yeast_matchpaper.R
@@ -5,6 +5,7 @@ do_yeast_matchpaper = function(yeast_counts){
 }
 
 do_yeast_remove0 = function(yeast_counts){
+  yeast_counts[yeast_counts == 0] = NA
   yeast_counts = log(yeast_counts)
   yeast_cor = cor(yeast_counts, use = "pairwise.complete.obs")
   yeast_cor
