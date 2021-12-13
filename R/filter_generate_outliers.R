b304365d1fb82e8afd7c73b09cdcba4c0ecfc359
rmflight <r.flight79@gmail.com>
2021-12-13 15:36:51 -0500

    trying another way to do the outliers

--- R/filter_generate_outliers.R
+++ R/filter_generate_outliers.R
@@ -17,7 +17,8 @@ filter_generate_outliers = function(counts, info, keep_num, sample_col, class_co
   counts_cor = run_cor_everyway(counts_filter, counts_completeness)
   counts_medians = calculate_cor_medians(counts_cor, info[[sample_col]], info[[median_col]])
   
-  counts_outliers = purrr::map(counts_medians, determine_outliers)
-  counts_outliers$features = rownames(counts_filter)
-  counts_outliers
+  counts_outliers = purrr::map_df(counts_medians, determine_outliers)
+  counts_outliers$keep_num = keep_num
+  list(outliers = counts_outliers,
+       features = rownames(counts))
 }
