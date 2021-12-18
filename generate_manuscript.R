f8886412bcab5378c1d564f44dbe1ff11e98db6c
rmflight <r.flight79@gmail.com>
2021-12-17 22:59:48 -0500

    complete reorg where we run supplemental first so we have access to the figure and table numbers

--- generate_manuscript.R
+++ generate_manuscript.R
@@ -1,6 +1,9 @@
+# for the manuscript
 source("./packages.R")
-
-## Load your R files
 lapply(list.files("./R", full.names = TRUE), source)
-
+# we process the supplement firs so we can refer to the figures and tables
+# in there in the main manuscript
+rmarkdown::render("./doc/supplemental_materials.Rmd")
 rmarkdown::render("./doc/ici_kt_manuscript.Rmd")
+
+beepr::beep(2)
