3cbb4c2ac9bc43fa6c2a62c94b4923517595058d
rmflight <r.flight79@gmail.com>
2022-02-09 19:08:28 -0500

    and all of the binary outputs

--- generate_manuscript.R
+++ generate_manuscript.R
@@ -1,7 +1,7 @@
 # for the manuscript
 source("./packages.R")
 lapply(list.files("./R", full.names = TRUE), source)
-# we process the supplement firs so we can refer to the figures and tables
+# we process the supplement first so we can refer to the figures and tables
 # in there in the main manuscript
 rmarkdown::render("./doc/supplemental_materials.Rmd")
 #beepr::beep(2)
