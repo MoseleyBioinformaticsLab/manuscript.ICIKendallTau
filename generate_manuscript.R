126ea59fa2d8e7fb911858603f21845201f542da
rmflight <r.flight79@gmail.com>
2022-02-18 10:05:23 -0500

    Pearson nolog incorporated

--- generate_manuscript.R
+++ generate_manuscript.R
@@ -4,6 +4,7 @@ lapply(list.files("./R", full.names = TRUE), source)
 # we process the supplement first so we can refer to the figures and tables
 # in there in the main manuscript
 rmarkdown::render("./doc/supplemental_materials.Rmd")
+rmarkdown::render("./doc/supplemental_table_1.Rmd")
 #beepr::beep(2)
 rmarkdown::render("./doc/ici_kt_manuscript.Rmd")
 
