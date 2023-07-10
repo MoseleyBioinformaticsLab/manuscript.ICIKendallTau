68f779c789428262c65900d3b61fdcec94a1f823
rmflight <r.flight79@gmail.com>
2023-07-10 13:26:37 -0400

    doing better at grouping

--- R/median_n_present.R
+++ R/median_n_present.R
@@ -13,6 +13,8 @@ group_study = function(counts_info, id)
   counts_long = counts_df |>
     tidyr::pivot_longer(cols = -feature, names_to = "sample", values_to = "count")
   counts_long = dplyr::left_join(counts_long, counts_info$info[, c("sample", "treatment")], by = "sample")
+  counts_long = counts_long |>
+    dplyr::filter(!is.na(treatment))
   
   group_medians = counts_long |>
     dplyr::group_by(treatment, feature) |>
@@ -38,17 +40,17 @@ group_study = function(counts_info, id)
 correlate_medians_n_present = function(grouped_medians,
                                        max_quantile = 0.5)
 {
-  # grouped_medians = tar_read(group_medians_adenocarcinoma)
+  # grouped_medians = tar_read(group_medians_yeast)
   median_npresent = grouped_medians$medians |>
-    dplyr::mutate(log_median = log2(median_present)) |>
+    dplyr::mutate(log_median = log10(median_present)) |>
     dplyr::filter(!is.na(median_present))
-  quantile_cut = quantile(median_npresent$log_median, probs = max_quantile, na.rm = TRUE)
+  quantile_cut = 10^quantile(median_npresent$log_median, probs = max_quantile, na.rm = TRUE)
   median_under_q = median_npresent |>
-    dplyr::filter(log_median <= quantile_cut)
+    dplyr::filter(median <= quantile_cut)
   median_treatment = split(median_under_q, median_under_q$treatment)
   cor_median = purrr::imap(median_treatment, function(x, y){
     out_cor = suppressWarnings(ICIKendallTau::ici_kt(x$n_present, x$median_present, perspective = "global"))
-    tibble::tibble(cor = out_cor["tau"], pvalue = out_cor["pvalue"], group = y)
+    tibble::tibble(cor = out_cor["tau"], pvalue = out_cor["pvalue"], treatment = y)
   }) |>
     dplyr::bind_rows()
   
@@ -57,9 +59,14 @@ correlate_medians_n_present = function(grouped_medians,
   min_treatment = split(median_min, median_min$treatment)
   cor_min = purrr::imap(min_treatment, function(x, y){
     out_cor = suppressWarnings(ICIKendallTau::ici_kt(x$n_present, x$min_median, perspective = "global"))
-    tibble::tibble(cor = out_cor["tau"], pvalue = out_cor["pvalue"], group = y)
+    tibble::tibble(cor = out_cor["tau"], pvalue = out_cor["pvalue"], treatment = y)
   }) |>
     dplyr::bind_rows()
-  list(medians = cor_median,
-       median_min = cor_min)
+  return(list(medians = list(medians = grouped_medians$medians,
+                      correlation = cor_median),
+       median_min = list(medians = grouped_medians$median_min,
+                         correlation = cor_min),
+       quantile = quantile_cut)
+
+  )
 }
