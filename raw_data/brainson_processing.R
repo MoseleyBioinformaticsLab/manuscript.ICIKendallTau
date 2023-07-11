0bd38b4a489b1ebe8d050f95478b4c9a1b506d51
rmflight <r.flight79@gmail.com>
2023-07-10 20:41:56 -0400

    close #16

--- raw_data/brainson_processing.R
+++ raw_data/brainson_processing.R
@@ -4,7 +4,7 @@ suppressPackageStartupMessages(library(DESeq2))
 brainsonrnaseq_counts = readRDS(here::here("raw_data","brainson_rnaseq", "brainson_rnaseq201901_counts.rds"))
 brainsonrnaseq_info = readRDS(here::here("raw_data", "brainson_rnaseq", "brainson_rnaseq201901_info.rds"))
 brainsonrnaseq_info = brainsonrnaseq_info |>
-  dplyr::mutate(treatment = tumor)
+  dplyr::mutate(treatment = type)
 brainsonrnaseq_counts = brainsonrnaseq_counts[, brainsonrnaseq_info$sample]
   
 deseq_counts = DESeqDataSetFromMatrix(round(brainsonrnaseq_counts), brainsonrnaseq_info, design = ~ treatment)
