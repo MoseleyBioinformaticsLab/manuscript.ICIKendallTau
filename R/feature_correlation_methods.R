980c77975930f085ec1059669a9b30ae76cd011e
rmflight <r.flight79@gmail.com>
2023-09-15 13:33:14 -0400

    enabling pairwise.complete for all the pearson calculations
    closes #18

--- R/feature_correlation_methods.R
+++ R/feature_correlation_methods.R
@@ -9,7 +9,7 @@ ici = function(counts_info, id, keep_num, sample_col, class_col)
   # keep_num = 1
   # sample_col = "sample"
   # class_col = "treatment"
-  n_workers = future::nbrOfWorkers()
+  # n_workers = future::nbrOfWorkers()
   if (n_workers == 80) {
     future::plan(multicore(workers = 50))
   }
@@ -23,9 +23,9 @@ ici = function(counts_info, id, keep_num, sample_col, class_col)
     filter_col = class_col
     median_col = class_col
   }
-  if (is.null(rownames(counts))) {
-    rownames(counts) = paste0("f", seq_len(nrow(counts)))
-  }
+  # if (is.null(rownames(counts))) {
+  #   rownames(counts) = paste0("f", seq_len(nrow(counts)))
+  # }
   counts_filter = t(keep_non_zero_percentage(t(counts), sample_classes = info[[filter_col]],
                                              keep_num = keep_num))
   tmp_out = ici_kendalltau(counts_filter, global_na = c(NA, 0), return_matrix = FALSE)
@@ -118,7 +118,7 @@ pearson_base_nozero = function(counts_info, id, keep_num, sample_col, class_col)
   counts_filter_na = counts_filter
   counts_filter_na[counts_filter == 0] = NA
   # this one should match the Gierlinski paper values for median correlations
-  tmp_out = cor(t(counts_filter_na), method = "pearson", use = "pairwise.complete")
+  tmp_out = pearson_wrapper(t(counts_filter_na))
   list(cor = tmp_out,
        data_id = counts_info$data_id,
        method_id = "pearson_base_nozero",
@@ -140,7 +140,7 @@ pearson_base = function(counts_info, id, keep_num, sample_col, class_col)
   }
   counts_filter = t(keep_non_zero_percentage(t(counts), sample_classes = info[[filter_col]],
                                              keep_num = keep_num))
-  tmp_out = cor(t(counts_filter), method = "pearson", use = "pairwise.complete")
+  tmp_out = pearson_wrapper(t(counts_filter))
   list(cor = tmp_out,
        data_id = counts_info$data_id,
        method_id = "pearson_base",
@@ -167,7 +167,7 @@ pearson_log1p = function(counts_info, id, keep_num, sample_col, class_col)
   }
   counts_filter = t(keep_non_zero_percentage(t(counts), sample_classes = info[[filter_col]],
                                              keep_num = keep_num))
-  tmp_out = cor(log1p(t(counts_filter)), method = "pearson", use = "pairwise.complete")
+  tmp_out = pearson_wrapper(log1p(t(counts_filter)))
   list(cor = tmp_out,
        data_id = counts_info$data_id,
        method_id = "pearson_log1p",
@@ -191,10 +191,99 @@ pearson_log = function(counts_info, id, keep_num, sample_col, class_col)
                                              keep_num = keep_num))
   log_counts = log(counts_filter)
   log_counts[is.infinite(log_counts)] = NA
-  tmp_out = cor(t(log_counts), method = "pearson", use = "pairwise.complete")
+  tmp_out = pearson_wrapper(t(log_counts))
   list(cor = tmp_out,
        data_id = counts_info$data_id,
        method_id = "pearson_log",
        full_id = id)
 }
 
+pearson_wrapper = function(data_matrix)
+{
+  n_sample = ncol(data_matrix)
+  if ("furrr" %in% utils::installed.packages()) {
+    ncore = future::nbrOfWorkers()
+    names(ncore) = NULL
+    split_fun = furrr::future_map
+  } else {
+    ncore = 1
+    split_fun = purrr::map
+  }
+  
+  # generate the array of comparisons, 2 x ...,
+  # where each column is a comparison between two columns of data
+  message("Figuring out comparisons to do ...")
+  pairwise_comparisons = utils::combn(n_sample, 2)
+  
+  extra_comparisons = matrix(rep(seq(1, n_sample), each = 2), nrow = 2, ncol = n_sample, byrow = FALSE)
+  pairwise_comparisons = cbind(pairwise_comparisons, extra_comparisons)
+  
+  # create a data.frame of the comparisons by the names of the columns instead,
+  # this enables indexed comparisons and named comparisons, because
+  # we can have row / column names in R
+  # This is now n_comparisons x 2
+  named_comparisons = data.frame(s1 = colnames(data_matrix)[pairwise_comparisons[1, ]],
+                                 s2 = colnames(data_matrix)[pairwise_comparisons[2, ]])
+  
+  
+  n_todo = nrow(named_comparisons)
+  
+  # in my experience, the list of actual comparisons to do does not get split
+  # up well across compute cores (cores, machines, etc), and we end up waiting
+  # on stuff to complete.
+  # Therefore, this code actually does the splitting up of comparisons across
+  # the number of cores (ncore) in a list. 
+  
+  message("Splitting up across compute ...")
+  n_each = ceiling(n_todo / ncore)
+  
+  which_core = rep(seq(1, ncore), each = n_each)
+  which_core = which_core[1:nrow(named_comparisons)]
+  
+  which_core = sample(which_core, length(which_core))
+  
+  named_comparisons$core = which_core
+  named_comparisons$raw = Inf
+  named_comparisons$pvalue = Inf
+  named_comparisons$taumax = Inf
+  
+  
+  split_comparisons = split(named_comparisons, named_comparisons$core)
+  
+  
+  # finally, this is the function for doing each set of icikt comparisons,
+  # possibly across cores.
+  do_split = function(do_comparisons, data_matrix) {
+    #seq_range = seq(in_range[1], in_range[2])
+    #print(seq_range)
+    
+    raw = vector("numeric", nrow(do_comparisons))
+    
+    
+    for (irow in seq_len(nrow(do_comparisons))) {
+      iloc = do_comparisons[irow, 1]
+      jloc = do_comparisons[irow, 2]
+      cor_res = cor(data_matrix[, iloc], data_matrix[, jloc], use = "pairwise.complete", method = "pearson")
+      raw[irow] = cor_res
+    }
+    do_comparisons$raw = raw
+    #return(ls())
+    do_comparisons
+  }
+  # we record how much time is actually spent doing ICI-Kt
+  # itself, as some of the other operations will add a bit of time
+  # 
+  message("Running correlations ...")
+  t1 = Sys.time()
+  # note here, this takes our list of comparisons, and then calls the do_split
+  # function above on each of them.
+  split_cor = split_fun(split_comparisons, do_split, data_matrix)
+  t2 = Sys.time()
+  t_diff = as.numeric(difftime(t2, t1, units = "secs"))
+  
+  # put all the results back together again into one data.frame
+  message("Recombining results ...")
+  all_cor = purrr::list_rbind(split_cor)
+  rownames(all_cor) = NULL
+  all_cor
+}
