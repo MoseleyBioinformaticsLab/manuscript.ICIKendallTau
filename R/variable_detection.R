6d2ad6fcab81b294cee408d9c22f15b1c4cbde63
rmflight <r.flight79@gmail.com>
2024-09-06 16:34:46 -0400

    updated to generate different amounts of cutoff
    related to #31

--- R/variable_detection.R
+++ R/variable_detection.R
@@ -1,28 +1,29 @@
-create_variable_lod_samples = function(n_feature, n_sample)
+create_variable_lod_samples = function(n_feature, n_sample,
+                                       variables = c(0.25, 0.5, 1, 1.5))
 {
   # n_feature = 1000
   # n_sample = 100
+  # variables = c(0.25, 0.5, 1, 1.5)
   base_sample = rlnorm(n_feature, meanlog = 1, sdlog = 0.5)
   rep_data = add_uniform_noise(n_sample, base_sample, 0.2)
   colnames(rep_data) = paste0("S", seq_len(ncol(rep_data)))
-  actual_ici = ici_kendalltau(t(rep_data), scale_max = FALSE)
   
-  exp_data = exp(rep_data)
-  exp_ici = ici_kendalltau(t(exp_data), scale_max = FALSE)
+  base_value = 2.1
   
-  actual_pearson = cor(rep_data)
-  exp_pearson = cor(exp_data)
+  base_value_matrix = matrix(base_value, nrow = nrow(rep_data), ncol = ncol(rep_data), byrow = TRUE)
   
+  rep_base = rep_data
+  rep_base[rep_data < base_value_matrix] = NA
   
-  cutoff_dist = runif(n = n_sample, min = min(rep_data), max = median(rep_data))
-  cutoff_matrix = matrix(cutoff_dist, nrow = nrow(rep_data), ncol = ncol(rep_data), byrow = TRUE)
+  variable_cutoffs = rep(variables * base_value, each = n_sample / length(variables))
   
-  cutoff_rep = rep_data
-  cutoff_rep[rep_data < cutoff_matrix] = NA
+  variable_value_matrix = matrix(variable_cutoffs, nrow = nrow(rep_data), ncol = ncol(rep_data), byrow = TRUE)
   
-  cutoff_exp = exp(cutoff_rep)
-  rep_exp = exp(rep_data)
+  rep_variable = rep_data
+  rep_variable[rep_data < variable_value_matrix] = NA
   
-  return(list(cutoff = cutoff_exp,
-              full = rep_exp))
+  return(list(cutoffs = exp(variable_cutoffs),
+              no_cutoff = exp(rep_data),
+              single_cutoff = exp(rep_base),
+              variable_cutoff = exp(rep_variable)))
 }
