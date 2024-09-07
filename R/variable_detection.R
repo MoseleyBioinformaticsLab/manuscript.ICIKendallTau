5990d1dca3cc49f91b437fc334fc80d2fc87f229
rmflight <r.flight79@gmail.com>
2024-09-06 21:42:37 -0400

    minor edits
    related to #29

--- R/variable_detection.R
+++ R/variable_detection.R
@@ -1,4 +1,5 @@
 create_variable_lod_samples = function(n_feature, n_sample,
+                                       base_value = 2.1,
                                        variables = c(0.25, 0.5, 1, 1.5))
 {
   # n_feature = 1000
@@ -7,8 +8,6 @@ create_variable_lod_samples = function(n_feature, n_sample,
   base_sample = rlnorm(n_feature, meanlog = 1, sdlog = 0.5)
   rep_data = add_uniform_noise(n_sample, base_sample, 0.2)
   colnames(rep_data) = paste0("S", seq_len(ncol(rep_data)))
-  
-  base_value = 2.1
   
   base_value_matrix = matrix(base_value, nrow = nrow(rep_data), ncol = ncol(rep_data), byrow = TRUE)
   
@@ -22,8 +21,19 @@ create_variable_lod_samples = function(n_feature, n_sample,
   rep_variable = rep_data
   rep_variable[rep_data < variable_value_matrix] = NA
   
-  return(list(cutoffs = exp(variable_cutoffs),
+  return(list(base_cutoff = exp(base_value),
+              variables = variables,
+              cutoffs = exp(variable_cutoffs),
               no_cutoff = exp(rep_data),
               single_cutoff = exp(rep_base),
               variable_cutoff = exp(rep_variable)))
+}
+
+
+calculate_variable_correlations = function(var_lod_samples)
+{
+  # tar_load(var_lod_samples)
+  true_ici_cor = ici_kendalltau(t(var_lod_samples$no_cutoff), scale_max = TRUE)$raw
+  
+  
 }
